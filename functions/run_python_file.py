bd369ae0a47b93e79ed1933ecf823585fadc48bd
krunyon7 <krunyon7@gmail.com>
2025-12-04 12:13:37 -0500

    update run_python_file and add related files

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -9,8 +9,23 @@ def run_python_file(working_directory, file_path, args=[]):
         return f'Error: File "{file_path}" not found.'
     if not target_file_path.endswith(".py"):
         return f'Error: "{file_path}" is not a Python file.'
+    sub_args = ["python3", target_file_path] + args
     try:
-        completed_process = subprocess.run(args, capture_output=True, cwd=abs_working_dir, timeout=30)
-        return f'STDOUT: {completed_process.stdout} STDERR: {completed_process.stderr}'
+        completed_process = subprocess.run(
+            sub_args, 
+            capture_output=True,
+            text=True, 
+            cwd=abs_working_dir, 
+            timeout=30,
+            check=True
+        )
+        result = []
+        if completed_process.stdout:
+            result.append(f"STDOUT:\n {completed_process.stdout}") 
+        if completed_process.stderr:
+            result.append(f"STDERR: {completed_process.stderr}")
+        if completed_process.returncode != 0:
+            result.append(f"Process exited with code {completed_process.returncode}")
+        return "\n".join(result) if result else "No output produced."
     except Exception as e:
         return f'Error: Executing Python file: {e}'
