2ea2ae331b3b28b4c308bacfeb1941b7f8da64a9
krunyon7 <krunyon7@gmail.com>
2025-12-04 13:44:31 -0500

    update agent to call permitted functions

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import argparse
 import os
 
+from prompts import system_prompt
+from call_function import available_functions
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+
 
 
 def main():
@@ -26,9 +30,12 @@ def main():
 
 
 def generate_content(client, messages, verbose):
+    
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt),
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
@@ -36,8 +43,14 @@ def generate_content(client, messages, verbose):
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+        return
+    
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":
